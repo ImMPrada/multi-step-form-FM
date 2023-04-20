166168dae8e37a26b6796a2966411fa8a0dee06d
immprada <im.mprada@gmail.com>
2023-04-20 11:26:04 -0500

    update

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,26 @@
+class AccountsController < ApplicationController
+  def new
+    @steps = [
+      {
+        number: 1,
+        title: 'YOUR INFO',
+        is_current_step: true
+      },
+      {
+        number: 2,
+        title: 'SELECT PLAN',
+        is_current_step: false
+      },
+      {
+        number: 3,
+        title: 'ADD-ONS',
+        is_current_step: false
+      },
+      {
+        number: 4,
+        title: 'SUMMARY',
+        is_current_step: false
+      }
+    ]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "forms#index"
+  root "accounts#new"
 
-  resources :forms, only: [:index]
+  resources :accounts, only: [:new, :create]
+
+  resource :account, only: [:show] do
+    resources :plans, only: [:new, :create]
+    resources :addo_ons, only: [:new, :create]
+    resources :payments, only: [:new, :create]
+  end
 end
