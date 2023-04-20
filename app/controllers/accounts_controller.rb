24accdf696d904998fd5bb30e3ffd4a3e953ddc6
immprada <im.mprada@gmail.com>
2023-04-20 16:29:46 -0500

    accounts#select_plan

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,10 +1,37 @@
 class AccountsController < ApplicationController
   def new
+    steps_current_step(1)
+
+    @account = Account.new
+  end
+
+  def create
+    @account = Account.new(account_params)
+
+    if @account.save
+      redirect_to select_plan_account_path(@account)
+    end
+  end
+
+  def select_plan
+    steps_current_step(2)
+
+    cookies[:plans_monthly] = Plan.where('active = ? AND frequence_unit = ?', true, 'mo').select(:name, :note, :frequence_number, :frequence_unit)
+    cookies[:plans_yearly] = Plan.where('active = ? AND frequence_unit = ?', true, 'yr').select(:name, :note, :frequence_number, :frequence_unit)
+  end
+
+  private
+
+  def account_params
+    params.require(:account).permit(:name, :email, :phone)
+  end
+
+  def steps_current_step(current_step_number)
     @steps = [
       {
         number: 1,
         title: 'YOUR INFO',
-        is_current_step: true
+        is_current_step: false
       },
       {
         number: 2,
@@ -23,6 +50,10 @@ class AccountsController < ApplicationController
       }
     ]
 
-    @account = Account.new
+    @steps.each do |step|
+      if step[:number] == current_step_number
+        step[:is_current_step] = true
+      end
+    end
   end
 end
