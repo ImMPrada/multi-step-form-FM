7ebb78219316458e6766cea6d496ed08a8c091ed
immprada <im.mprada@gmail.com>
2023-05-06 11:30:54 -0500

    add Addon

---
+++ spec/models/addon_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Addon, type: :model do
+  subject(:addon) { build(:addon) }
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:price) }
+    it { is_expected.to validate_presence_of(:status) }
+  end
+
+  describe 'associations' do
+    it { is_expected.to have_many(:account_addons).dependent(:destroy) }
+    it { is_expected.to have_many(:accounts).through(:account_addons) }
+    it { is_expected.not_to have_many(:accounts).through(:account_addons).dependent(:destroy) }
+  end
+end
