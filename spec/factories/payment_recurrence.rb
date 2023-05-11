04ebcb47c3f6c8d396750f367d63e2e0bc048378
immprada <im.mprada@gmail.com>
2023-05-11 17:19:23 -0500

    update models

--- spec/factories/payment_recurrence.rb
+++ spec/factories/payment_recurrence.rb
@@ -1,7 +1,9 @@
 FactoryBot.define do
   factory :payment_recurrence do
     type { %w[yearly monthly weekly daily].sample }
-    frecuence { Faker::Number.between(from: 1.0, to: 10.0) }
     acronym { Faker::Lorem.characters(number: 3) }
+    days_delta { Faker::Number.between(from: 0.0, to: 30.0) }
+    months_delta { Faker::Number.between(from: 0.0, to: 12.0) }
+    years_delta { Faker::Number.between(from: 0.0, to: 10.0) }
   end
 end
