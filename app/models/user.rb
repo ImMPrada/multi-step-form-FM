292ee2420346facc0ccaee4e09f5545369ec200c
immprada <im.mprada@gmail.com>
2023-05-23 16:08:59 -0500

    implement password fild validations via devise gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable # , :validatable
+         :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: {
@@ -10,7 +10,7 @@ class User < ApplicationRecord
     message: 'invalid email address'
   }
   validates :phone_number, presence: true
-  validates :password, presence: true, confirmation: { message: 'passwords must match' }
+  validates :password, presence: true
   validates :password_confirmation, presence: true
 
   has_many :onboardings, dependent: :destroy, foreign_key: :owner_id
