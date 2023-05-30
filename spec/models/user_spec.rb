37c87ac2dcc4133b316994e84da93ff2f602bf99
immprada <im.mprada@gmail.com>
2023-05-29 21:15:07 -0500

    remove an example from User model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
     it { is_expected.to validate_presence_of(:password) }
     it { is_expected.to validate_presence_of(:password_confirmation) }
-    it { is_expected.to validate_confirmation_of(:password).with_message('passwords must match') }
   end
 
   describe 'associations' do
