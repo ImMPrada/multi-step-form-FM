60ae994eba856e374b072d3122b4ffd30971e4f8
immprada <im.mprada@gmail.com>
2023-06-10 19:03:24 -0500

    create addons into account

--- app/controllers/addons_controller.rb
+++ app/controllers/addons_controller.rb
@@ -10,10 +10,35 @@ class AddonsController < ApplicationController
   end
 
   def create
-    byebug
+    empty_account_addons
+    addons_params.each do |_checkbox_id, addoin_id|
+      add_addon_to_account(addoin_id) unless addoin_id.to_i.zero?
+    end
   end
 
   private
+
+  def addons_params
+    addons_ids = []
+    addons.each do |addon|
+      addons_ids << "addon_#{addon.id}".to_sym
+    end
+    params.require(:account).permit(addons_ids)
+  end
+
+  def add_addon_to_account(addoin_id)
+    account_addon = AccountAddon.new(account:, addon_id: addoin_id)
+    account_addon.save
+  end
+
+  def empty_account_addons
+    account_addons = AccountAddon.where(account:)
+    account_addons.destroy_all unless account_addons.empty?
+  end
+
+  def addons
+    @addons ||= Addon.all
+  end
 
   def account
     @account ||= onboarding.account
@@ -22,5 +47,4 @@ class AddonsController < ApplicationController
   def onboarding
     @onboarding ||= Onboarding.find(params[:onboarding_id])
   end
-
 end
